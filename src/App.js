6bb7fd11f9c5eb24bcffb0e642fc47a346f7fa42
Owner <shayleebaumgartner2002@gmail.com>
2024-07-09 15:35:54 -0400

    switching browserRouter HashRouter since gh-pages doesn't support it

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import VendingMachine from './components/VendingMachine';
 import Doritos from './components/snacks/Doritos';
 import Coke from './components/snacks/Coke';
@@ -10,7 +10,7 @@ import './css/App.css';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path='/' element={<VendingMachine/>}/>
           <Route path='/coke' element={<Coke/>}/>
@@ -18,7 +18,7 @@ function App() {
           <Route path='/snickers' element={<Snickers/>}/>
           <Route path='/racoon' element={<Racoon/>}/>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
